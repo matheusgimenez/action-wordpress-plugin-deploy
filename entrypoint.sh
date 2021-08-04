b05b2219fcb57f99bb4f45953ff6096daf5e6aeb
Matheus Gimenez <contato@matheusgimenez.com.br>
2021-08-04 09:57:38 -0300

    add .wporg-ignore to ignore files on build to wporg

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,20 +76,18 @@ else
 	git config --global user.name "10upbot on GitHub"
 
 	# If there's no .gitattributes file, write a default one into place
-	if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
-		cat > "$GITHUB_WORKSPACE/.gitattributes" <<-EOL
+	if [[ ! -e "$GITHUB_WORKSPACE/src/.gitattributes" ]]; then
+		cat > "$GITHUB_WORKSPACE/src/.gitattributes" <<-EOL
 		/$ASSETS_DIR export-ignore
 		/.gitattributes export-ignore
-		/.gitignore export-ignore
+		/.wporg-ignore export-ignore
 		/.github export-ignore
 		EOL
 
 		# Ensure we are in the $GITHUB_WORKSPACE directory, just in case
 		# The .gitattributes file has to be committed to be used
 		# Just don't push it to the origin repo :)
-		cd "$GITHUB_WORKSPACE"
 		git add .gitattributes && git commit -m "Add .gitattributes file"
-		cd "$GITHUB_WORKSPACE/src";
 	fi
 
 	# This will exclude everything in the .gitattributes file with the export-ignore flag
